9a1da9d5a4bad3e76ddff6f4cf3d3b4d0579e626
Bruno Silva <bruno3dcontato@gmail.com>
2022-01-06 23:47:49 -0300

    refactor: remove node-canvas, add useMemo to hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,51 @@
-import { decode } from "blurhash";
-import { createCanvas } from "canvas";
+import { useMemo } from 'react';
+import { decode } from 'blurhash';
 
-export default function useBlurData(
-  blurHash: string,
+function blurhashToDataURL(
+  blurhash: string,
   width: number = 160,
   height: number = 120,
   punch?: number
 ) {
-  const pixels = decode(blurHash, width, height, punch);
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext("2d");
-  const imageData = ctx.createImageData(width, height);
-  imageData.data.set(pixels);
-  ctx.putImageData(imageData, 0, 0);
-  const blurDataUrl = canvas.toDataURL();
+  const w = Math.round(width || 100);
+  const h = Math.round(height || 100);
 
-  return [blurDataUrl];
+  const pixels = decode(blurhash, w, h, punch);
+  const canvas = document.createElement('canvas');
+
+  canvas.width = w;
+  canvas.height = h;
+
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    canvas.remove();
+    throw Error('Invalid 2D Context');
+  }
+
+  const idata = ctx.createImageData(w, h);
+
+  idata.data.set(pixels);
+
+  ctx.putImageData(idata, 0, 0);
+
+  const base64 = canvas.toDataURL();
+
+  canvas.remove();
+
+  return base64;
+}
+
+export default function useBlurData(
+  blurhash: string,
+  width: number = 160,
+  height: number = 120,
+  punch?: number
+) {
+  return useMemo(() => {
+    if (typeof window === 'undefined') {
+      return undefined;
+    }
+    return blurhashToDataURL(blurhash, width, height, punch);
+  }, []);
 }
